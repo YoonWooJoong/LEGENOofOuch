5d0ff04cbf4d5af086a8718de601c017390e4607
dongseok-kim <kdsehdtjr12@gmail.com>
2025-02-21 19:40:51 +0900

    Feat:Enum
    +MonsterEnum:Enum
    +PlayerClassEnum:Enum
    +RankEnum:Enum
    +UIEnum:Enum
    +StageEnum:Enum +AbilityEnum:Enum
    각자 클래스에서 사용할 기본 Enum추가

---
+++ Assets/02_Scripts/Enum.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum MonsterEnum
+{
+    Slime,
+    Orc,
+    Skeleton
+}
+
+public enum PlayerClassEnum
+{
+    Warrior,
+    Mage,
+    Archer
+}
+
+public enum RankEnum
+{
+    Normal,
+    Rare
+}
+
+public enum UIEnum
+{    
+    GameStartUI
+}
+
+public enum StageEnum
+{
+    Castle,
+    Swamp,
+    volcano
+}
+public enum AbilityEnum
+{
+    AttackBoost,
+    AttackSpeedBoost,
+    SpeedBoost,
+    CriticalMaster,
+    Criticalaura,
+    HPBoost,
+    Fury,
+    DeathBoom,
+    DeathNova,
+    EnhancedRecovery,
+    BloodThirst,
+    Invincibility,
+    Recoil,
+    WallReflection,
+    PiercingShot,
+    MultiShot,
+    FrontShot,
+    DiagonalShot,
+    SideShot,
+    RearShot,
+    Blaze,
+    SacredTouch,
+    DarkTouch,
+    ShieldGuard,
+    FireOrb,
+    Spirit,
+    ObsidianOrb,
+    ExtraLife,
+    Archer,
+    Mage,
+    Warrior
+}
