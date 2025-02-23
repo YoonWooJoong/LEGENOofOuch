efa0b10fda31f18a9edc0520fa1574aff87707c5
tmdrb7214 <tmdrb7214@naver.com>
2025-02-23 23:19:59 +0900

    [Feat] 캐릭터선택 , Start화면에서 선택된캐릭터미리보기

---
+++ Assets/02_Scripts/CharacterSelceted.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelcet : MonoBehaviour
+{
+
+    public Image characterPreview; //선택된 캐릭터 미리보기
+    public Sprite[] characterImages; //캐릭터이미지 배열
+    public string[] characterNames; // 캐릭터 이름
+    public Text characterNameText; // 선택된 캐릭터 이름 표시
+
+    private int selectedCharacterIndex = 0; // 선택된 캐릭터 인덱스
+    void Start()
+    {
+  
+        LoadSavedCharater();
+    }
+
+
+    public void SelectCharater(int index)
+    {
+        selectedCharacterIndex = index;
+        characterPreview.sprite = characterImages[index];
+        characterNameText.text = characterNames[index];
+    }
+
+    public void SaveCharacter()
+    {
+        PlayerPrefs.SetInt("SelectedCharacter", selectedCharacterIndex);
+        PlayerPrefs.Save();
+        Debug.Log("Character Saved: " + characterNames[selectedCharacterIndex]);
+    }
+    
+    void LoadSavedCharater()
+    {
+        selectedCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+        SelectCharater(selectedCharacterIndex);
+    }
+
+}
--- Assets/02_Scripts/Managers/StageManager.cs
+++ Assets/02_Scripts/Managers/StageManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CharacterSelcet : MonoBehaviour
     {
-        
-    }
+        public Image StageImage; //스테이지 이미지 배열
+        public Text StageName; // 스테이지 이름
+        public Sprite[] StageImages; //스테이지 이미지 배열
+        public string[] StageNames; // 스테이지 이름
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private int StageIndex = 0; // 선택된 캐릭터 인덱스
+
+
+
+        void UpdateStageUI()
+        {
+            if (StageImages.Length > 0 && StageNames.Length > 0)
+            {
+                StageImage.sprite = StageImages[StageIndex];
+                StageName.text = StageNames[StageIndex];
+            }
+        }
+
+        public void NextStage()
+        {
+            StageIndex = (StageIndex + 1) % StageImages.Length;
+            UpdateStageUI();
+        }
+
+        public void PreviousStage()
+        {
+            StageIndex = (StageIndex - 1) % StageImages.Length;
+            UpdateStageUI();
+        }
+
+
+        void Start()
+        {
+            UpdateStageUI();
+        }
     }
 }
