368b219400cf551afcd7a37355b03e94cb8d1250
tmdrb7214 <tmdrb7214@naver.com>
2025-02-23 21:04:25 +0900

    Feat : 버튼으로 UI창띄우기

---
+++ Assets/02_Scripts/Managers/HomeUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HomeUI 
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button settingButton;
+    [SerializeField] private Button CharacterBttuon;
+}
