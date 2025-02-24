bd56033b957c1ebe12fa04065659042ff9303d48
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-02-24 15:12:43 +0900

    <fix>: 총알 구조 변경
    1. 전에 코드는 플레이어 입장에서만 작성했기 때문에 enemy도 사용할 수 있는 코드 작성 LayerMask사용
    2. ProjectileManager에 플레이어와 적이 사용하는 매서드 두개 생성
    3. 총알끼리 부딫혀도 넘어가게 설정
    4. 플레이어 총알이 플레이어한테 부딫혀도 통과
    5. 플레이어만 벽튕기는 횟수와 적 튕기는 횟수 받아옴
    6. 레이어 Player, Enemy, Wall 작성
    7. Fairy프리팹 생성

--- Assets/02_Scripts/Managers/ProjectileManager.cs
+++ Assets/02_Scripts/Managers/ProjectileManager.cs
@@ -6,15 +6,29 @@ using UnityEngine;
 public class ProjectileManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] projectilePrefabs;
+    [SerializeField] private GameObject monsterProjectilePrefabs;
 
     /// <summary>
     /// 총알 생성, 리스트에 워리어, 위자드, 궁수 순서대로 프리팹 넣어야함
     /// </summary>
-    /// <param name="startPosition">플레이어 위치</param>
+    /// <param name="startPosition">시작 위치</param>
     /// <param name="playerClass">플레이어 클래스</param>
-    public void ShootProjectile(Vector2 startPosition, PlayerClassEnum playerClass)
+    /// <param name="direction"></param>
+    public void ShootPlayerProjectile(Vector2 startPosition, Vector2 direction, PlayerClassEnum playerClass, int contactWallCount, int contactEnemyCount)
     {
         GameObject origin = projectilePrefabs[Convert.ToInt32(playerClass)];
         GameObject obj = Instantiate(origin, startPosition, Quaternion.identity);
+        
+        ProjectileController projectileController = obj.GetComponent<ProjectileController>();
+        projectileController.Init(direction, contactWallCount,contactEnemyCount);
+    }
+
+    public void ShootEnemyProjectile(Vector2 startPosition, Vector2 direction)
+    {
+        GameObject origin = monsterProjectilePrefabs;
+        GameObject obj = Instantiate(origin, startPosition, Quaternion.identity);
+
+        ProjectileController projectileController = obj.GetComponent<ProjectileController>();
+        projectileController.Init(direction);
     }
 }
--- Assets/02_Scripts/ProjectileController.cs
+++ Assets/02_Scripts/ProjectileController.cs
@@ -5,37 +5,45 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
+    [SerializeField] private LayerMask layerMaskEnemy; //적 설정
+    [SerializeField] private LayerMask layerMaskWall; // 벽 설정
     private Rigidbody2D rigidbody2D;
-    private Vector2 playerDirection;
-    public GameObject one;        //test용도
-    public GameObject two;        //test용도
-    private Vector2 direction;    //test용도
-    private int contactWall = 0; // 벽과 충돌 횟수
-    private int contactEnemy = 0; // 적과 충돌 횟수
+    private Vector2 direction;
+    private int contactWall; // 벽과 충돌 횟수
+    private int contactEnemy; // 적과 충돌 횟수
+    private int contactWallCount; // 받아온 벽 충돌 횟수
+    private int contactEnemyCount; // 받아온 적 충돌 횟수
 
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
-    private void Start()
+
+    /// <summary>
+    /// 총알 초기화 메서드
+    /// </summary>
+    /// <param name="_direction">방향</param>
+    /// <param name="_contactwallCount">벽 충돌횟수, 적이 쏘면 0 </param>
+    /// <param name="_contactEnemyCount">적 충돌횟수, 적이쏘면 0 </param>
+    public void Init(Vector2 _direction, int _contactwallCount = 0, int _contactEnemyCount = 0)
     {
-        // playerDirection = GameManager.Instance.Player.Direction; 
-        direction = one.transform.position - two.transform.position;
+        direction = _direction;
         RotationRojectile();
+        contactWallCount = _contactwallCount;
+        contactEnemyCount = _contactEnemyCount;
     }
+
     void Update()
     {
         DirectionProjcetile();
     }
-    
+
     /// <summary>
     /// 총알진행 메서드
     /// </summary>
     public void DirectionProjcetile()
     {
-        //rigidbody2D.velocity = playerDirection * 10f;
         rigidbody2D.velocity = direction.normalized * 10f;
-
     }
 
     /// <summary>
@@ -62,27 +70,60 @@ public class ProjectileController : MonoBehaviour
     /// <param name="collision">벽 혹은 적</param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (contactWall < 2 && collision.gameObject.CompareTag("Wall")) // 임시로 wall로 작성 // 숫자에는 총알 튕기는 횟수변수 넣어주면됨
+        if (collision.gameObject.GetComponent<ProjectileController>()) //다른 오브젝트가 ProjectileController를 가지고 있으면 총알이기 때문에
+        { Physics2D.IgnoreLayerCollision(this.gameObject.layer, collision.gameObject.layer); } // 무시
+        else // 그외
         {
-            var contact = collision.contacts[0]; // 충돌 지점
-            direction = Vector3.Reflect(direction, contact.normal); // 현재 진행방향과 충돌지점을 계산해 반사각을 구해줌
-            RotationRojectile();
-            contactWall += 1;
+            if (layerMaskWall.value == (layerMaskWall.value | (1 << collision.gameObject.layer))) // 벽과 충돌했을때
+            {
+                if (contactWall < contactWallCount) // 현재 충돌횟수가 받아온 충돌횟수보다 적다면
+                {
+                    var contact = collision.contacts[0];
+                    // 충돌 지점
+                    direction = Vector3.Reflect(direction, contact.normal); // 현재 진행방향과 충돌지점을 계산해 반사각을 구해줌
+                    RotationRojectile();
+                    contactWall += 1;
+                }
+                else if (contactWall >= contactWallCount) // 현재 충돌횟수가 받아온 충돌횟수와 같거나 크다면
+                    Destroy(this.gameObject);
+            }
+            else if (layerMaskEnemy.value == (layerMaskEnemy.value | (1 << collision.gameObject.layer))) // 적과 충돌했을때
+            {
+                if (contactEnemy < contactEnemyCount)
+                {
+                    var contact = collision.contacts[0];
+                    // 충돌 지점
+                    direction = Vector3.Reflect(direction, contact.normal); // 현재 진행방향과 충돌지점을 계산해 반사각을 구해줌
+                    RotationRojectile();
+                    contactEnemy += 1;
+                }
+                else if (contactEnemy >= contactEnemyCount)
+                    Destroy(this.gameObject);
+            }
+            else { Physics2D.IgnoreLayerCollision(this.gameObject.layer, collision.gameObject.layer); } // 같은 레이어는 무시
         }
-        else if (contactWall >= 2 && collision.gameObject.CompareTag("Wall")) // 임시로 wall로 작성
-        {
-            Destroy(this.gameObject);
-        }
-        else if (contactEnemy < 2 && collision.gameObject.CompareTag("Enemy")) // 임시로 Enemy로 작성 
-        {
-            var contact = collision.contacts[0]; // 충돌 지점
-            direction = Vector3.Reflect(direction, contact.normal); // 현재 진행방향과 충돌지점을 계산해 반사각을 구해줌
-            RotationRojectile();
-            contactEnemy += 1;
-        }
-        else if (contactEnemy >= 2 && collision.gameObject.CompareTag("Enemy")) // 임시로 Enemy로 작성
-        {
-            Destroy(this.gameObject);
-        }
+
+        //if (contactWall < 2 && collision.gameObject.CompareTag("Wall")) // 임시로 wall로 작성 // 숫자에는 총알 튕기는 횟수변수 넣어주면됨
+        //{
+        //    var contact = collision.contacts[0]; // 충돌 지점
+        //    direction = Vector3.Reflect(direction, contact.normal); // 현재 진행방향과 충돌지점을 계산해 반사각을 구해줌
+        //    RotationRojectile();
+        //    contactWall += 1;
+        //}
+        //else if (contactWall >= 2 && collision.gameObject.CompareTag("Wall")) // 임시로 wall로 작성
+        //{
+        //    Destroy(this.gameObject);
+        //}
+        //else if (contactEnemy < 2 && collision.gameObject.CompareTag("Enemy")) // 임시로 Enemy로 작성 
+        //{
+        //    var contact = collision.contacts[0]; // 충돌 지점
+        //    direction = Vector3.Reflect(direction, contact.normal); // 현재 진행방향과 충돌지점을 계산해 반사각을 구해줌
+        //    RotationRojectile();
+        //    contactEnemy += 1;
+        //}
+        //else if (contactEnemy >= 2 && collision.gameObject.CompareTag("Enemy")) // 임시로 Enemy로 작성
+        //{
+        //    Destroy(this.gameObject);
+        //}
     }
 }
