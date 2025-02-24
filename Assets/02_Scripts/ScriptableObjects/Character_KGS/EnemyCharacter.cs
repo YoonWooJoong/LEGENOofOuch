7ae7af09dd6f925dde02bf9467fba697f6e02b45
Kim-giseok <belbeck@naver.com>
2025-02-24 12:57:49 +0900

    Feat: 적 이동 구현
    사정거리 밖에 플레이어가 있으면 플레이어 방향으로 이동하고, 사정거리 안에 들어오면 공격합니다.

--- Assets/02_Scripts/ScriptableObjects/Character_KGS/EnemyCharacter.cs
+++ Assets/02_Scripts/ScriptableObjects/Character_KGS/EnemyCharacter.cs
@@ -1,7 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyCharacter : BaseCharacter
 {
+    [SerializeField] float attackRange;
+
+    void Start()
+    {
+        target = FindAnyObjectByType(typeof(PlayerCharacter)).GameObject().transform;
+    }
+
+    /// <summary>
+    /// 공격사거리보다 플레이어가 멀리 있으면 플레이어 쪽으로 이동합니다. 아니면 그자리에서 공격하도록 합니다.
+    /// </summary>
+    protected override void HandleAction()
+    {
+        base.HandleAction();
+        lookDir = (target.transform.position - transform.position).normalized;
+        if (TargetDis > attackRange)
+            moveDir = lookDir;
+        else
+            moveDir = Vector2.zero;
+    }
 }
