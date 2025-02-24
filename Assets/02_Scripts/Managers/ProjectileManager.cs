16b34233b657e6f534d52e213acf352436fcb7ab
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-02-24 11:48:47 +0900

    feat: 총알 생성
    1. 총알 생성 매서드

--- Assets/02_Scripts/Managers/ProjectileManager.cs
+++ Assets/02_Scripts/Managers/ProjectileManager.cs
@@ -1,18 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] projectilePrefabs;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 총알 생성, 리스트에 워리어, 위자드, 궁수 순서대로 프리팹 넣어야함
+    /// </summary>
+    /// <param name="startPosition">플레이어 위치</param>
+    /// <param name="playerClass">플레이어 클래스</param>
+    public void ShootProjectile(Vector2 startPosition, PlayerClassEnum playerClass)
     {
-        
+        GameObject origin = projectilePrefabs[Convert.ToInt32(playerClass)];
+        GameObject obj = Instantiate(origin, startPosition, Quaternion.identity);
     }
 }
