66d1408861d11b65e8a2bfd46dea32a5d7035250
Kim-giseok <belbeck@naver.com>
2025-02-25 10:32:22 +0900

    Feat: 플레이어 버프 반영
    버프수치를 반영해 인게임에서 플레이어의 최종 스텟을 표시합니다.

--- Assets/02_Scripts/Character/PlayerCharacter.cs
+++ Assets/02_Scripts/Character/PlayerCharacter.cs
@@ -10,6 +10,22 @@ public class PlayerCharacter : BaseCharacter
     [Header("")]
     [SerializeField] PlayerClassEnum pClass;
 
+    //버프수치: 20%증가시 0.2f 입력
+    public float MaxHpBuf { get; set; }
+    public float SpeedBuf { get; set; }
+    public float AtkBuf { get; set; }
+    public float AsBuf { get; set; }
+    public float CriDmgBuf { get; set; }
+    public float CriChanceBuf { get; set; }
+
+    public override float MaxHp => base.MaxHp * (1 + MaxHpBuf);
+    public override float Speed => base.Speed * (1 + SpeedBuf);
+    public override float AttackPower => base.AttackPower * (1 + AtkBuf);
+    public override float AttackSpeed => base.AttackSpeed * (1 + AsBuf);
+    public float CriDmg => criticalDamage + CriDmgBuf;
+    public float CriChance => criticalChance + CriChanceBuf;
+
+
     /// <summary>
     /// 키보드 입력으로 이동방향을 결정합니다.
     /// </summary>
@@ -41,4 +57,8 @@ public class PlayerCharacter : BaseCharacter
         //사망시 게임종료 로직 실행
         base.Death();
     }
+
+
+    //무적
+    //목숨추가
 }
