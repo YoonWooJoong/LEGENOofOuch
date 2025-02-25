e74afc3f4add5fbd57b149ed8c8a08c075b172c9
tmdrb7214 <tmdrb7214@naver.com>
2025-02-25 14:28:25 +0900

    Chore 이미지추가 , 수정

--- Assets/02_Scripts/Managers/UIManager.cs
+++ Assets/02_Scripts/Managers/UIManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    GameManager gameManager = new GameManager();
+    SelectManager selectManager;
     [SerializeField] private GameObject nextStageButton;
     [SerializeField] private GameObject previousStageButton;
 
@@ -30,18 +32,48 @@ public class UIManager : MonoBehaviour
     }
 
 
+    /// <summary>
+    /// 다음 스테이지 버튼
+    /// 마지막 스테이지에서는 버튼 비활성화
+    /// </summary>
     public void NextStageButton()
     {
-       int number = GameManager.Instance.SelectManager.GetSelectedStageIndex();
-        //if()
-        GameManager.Instance.SelectManager.SetSelectedStageIndex( number);
-    }
+        int number = gameManager.SelectManager.GetSelectedStageIndex();
+        if (number < 2)
+        {
+            int nextNumber = number + 1;
+            GameManager.Instance.SelectManager.SetSelectedStageIndex(nextNumber);
+            selectManager.UpdateStageUI();
 
+            previousStageButton.SetActive(true);
+            if (nextNumber == 2)
+            {
+                nextStageButton.SetActive(false);
+            }
+
+        }
+    }
+    /// <summary>
+    /// 이전 스테이지 버튼
+    /// 처음시작 
+    /// </summary>
     public void PreviousStageButton()
     {
+        int number = gameManager.SelectManager.GetSelectedStageIndex();
+        if (number > 0)
+        {
+            int nextnumber = number - 1;
+            GameManager.Instance.SelectManager.SetSelectedStageIndex(nextnumber);
+            selectManager.UpdateStageUI();
 
+            nextStageButton.SetActive(true);
+            if (nextnumber == 0)
+            {
+                previousStageButton.SetActive(false);
+            }
+
+        }
     }
-
 
     /*
     public void NextStage()
