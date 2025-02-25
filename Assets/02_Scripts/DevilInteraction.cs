5d1f05d517b38105a795a103e6a8d0f4b1002fd0
dongseok-kim <kdsehdtjr12@gmail.com>
2025-02-25 12:16:20 +0900

    Docs:주석추가
    DevilInteraction
    Trade에 주석추가

--- Assets/02_Scripts/DevilInteraction.cs
+++ Assets/02_Scripts/DevilInteraction.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DevilInteraction : MonoBehaviour
 {
     public GameObject tradeUI;  // 거래 UI를 참조하는 변수
 
-    // 악마와 충돌이 발생했을 때 (2D 충돌 처리)
+    /// <summary>
+    /// 악마라운드에서 플레이어가 악마와 접촉했을때 trade진행ㅇ
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // 충돌한 게임 오브젝트가 'Player' 태그를 가지고 있는지 확인
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("부딫!");
             // 거래 UI 활성화
             tradeUI.SetActive(true);
         }
--- Assets/02_Scripts/Trade.cs
+++ Assets/02_Scripts/Trade.cs
@@ -17,12 +17,22 @@ public class Trade : MonoBehaviour
         this.gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// 플레이어가 거래버튼을 눌렀을때 게임매니저에게 수락을 보내준다.
+    /// 게임매니저는 플레이어에게 extralife를 주고 생명력 30을 가져온다.
+    /// </summary>
     public void AcceptTrade()
     {
         Debug.Log("거래를 수락했습니다.");
+        //GameManager.instance.Trade();
+        this.gameObject.SetActive(false);
     }
+    /// <summary>
+    /// ui비활성화 되고 끝
+    /// </summary>
     public void RejectTrade()
     {
         Debug.Log("거래를 거절했습니다.");
+        this.gameObject.SetActive(false);
     }
 }
