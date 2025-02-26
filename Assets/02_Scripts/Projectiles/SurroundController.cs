9977ec1c9d8cd23f4e2b430ad78f7dabac169986
YoonWooJoong <dbsdnwnd1126@gmail.com>
2025-02-26 20:40:13 +0900

    fix: 요정 공격 시 nullreferenceException 뜨는 오류 해결 재업로드

--- Assets/02_Scripts/Projectiles/SurroundController.cs
+++ Assets/02_Scripts/Projectiles/SurroundController.cs
@@ -24,7 +24,11 @@ public class SurroundController : MonoBehaviour
         else if (IsAttacking)
         {
             timeSinceLastAttack = 0;
-            GameManager.Instance.ProjectileManager.ShootFairy(this.transform.position, (GameManager.Instance.player.target.position - this.transform.position));
+            if (GameManager.Instance.player.target == null)
+            {
+                Debug.Log("요정이 공격할 대상이 없습니다.");
+            }
+            else { GameManager.Instance.ProjectileManager.ShootFairy(this.transform.position, (GameManager.Instance.player.target.position - this.transform.position)); }
         }
 
     }
