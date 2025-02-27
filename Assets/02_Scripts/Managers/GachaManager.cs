accb85bf4b4997454faca7d7619b0ce453213764
dongseok-kim <kdsehdtjr12@gmail.com>
2025-02-27 20:39:43 +0900

    Chore:레어확률올림, 보스스테이지에서 가챠안뜨게 변경
    Chore:레어확률올림, 보스스테이지에서 가챠안뜨게 변경

--- Assets/02_Scripts/Managers/GachaManager.cs
+++ Assets/02_Scripts/Managers/GachaManager.cs
@@ -40,6 +40,7 @@ public class GachaManager : MonoBehaviour
         gachaHandler.init();
         gacha.SelectRandomAbility();
         GameManager.Instance.SetAbilityText();
+        if(GameManager.Instance.LevelManager.NowRound()<14)
         gachaHandler.StartGacha();
     }
 
